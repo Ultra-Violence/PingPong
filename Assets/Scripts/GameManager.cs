b1d0512a5de29183ece584cda94d21d4cfa3cc91
Ultra-Violence <PovarVitaliy00@gmail.com>
2023-08-01 00:56:20 +0300

    1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public GameObject playerPrefab;
+    public Text scoreText;
+    public Text ballsText;
+    public Text levelText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
